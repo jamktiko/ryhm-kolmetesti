1afd0d51a5e7148fa8c413a2a69d3814c2f0d0e6
Jarkko Remes <ag1534@student.jamk.fi>
2025-04-24 11:03:00 +0300

    Added h2 header and a paragraph to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Jarkon hieno h2 headeri</h2>
+<p>Ja myös Jarkon hieno kappale</p>
