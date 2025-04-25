e57c91f1b3604a01c79210f313cd1ea2455b9126
Sade, Haarala <sade.haarala@domain.fi>
2025-04-25 14:36:29 +0300

    joo

--- src/lib/components/WeatherMain.svelte
+++ src/lib/components/WeatherMain.svelte
@@ -17,21 +17,21 @@
 
 <h2>{returnedCity}</h2>
 {#if weather}
-	<p>Temperature {weather.current.temperature_2m} °C</p>
-	<p>Apparent temperature {weather.current.apparent_temperature} °C</p>
-	<p>Relative humidity {weather.current.relative_humidity_2m} %</p>
-	<p>Wind speed {weather.current.wind_speed_10m} m/s</p>
-	<p>Wind Direction {weather.current.wind_direction_10m} °</p>
-	<p>Cloud cover {weather.current.cloud_cover} %</p>
-	<p>Now is {weather.current.is_day ? 'Day' : 'Night'}</p>
-	<p>Rain {weather.current.rain} mm</p>
+	<p>Lämpötila {weather.current.temperature_2m} °C</p>
+	<p>Tuntuu kuin {weather.current.apparent_temperature} °C</p>
+	<p>Suhteellinen ilmankosteus {weather.current.relative_humidity_2m} %</p>
+	<p>Tuulennopeus {weather.current.wind_speed_10m} m/s</p>
+	<p>Tuulensuunta {weather.current.wind_direction_10m} °</p>
+	<p>Pilvisyys {weather.current.cloud_cover} %</p>
+	<p>Päivä {weather.current.is_day ? 'Day' : 'Night'}</p>
+	<p>Sateen määrä {weather.current.rain} mm</p>
 {:else}
 	<p>Ei säätietoja</p>
 {/if}
 
 <style>
 	h2 {
-		color: var(--main-color);
+		color: var(--text-decoration-color);
 	}
 	p {
 		color: var(--text-decoration-color);
