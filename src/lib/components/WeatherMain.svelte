2e22a756bfc691805a3113655ed371020ffddc05
Sade, Haarala <sade.haarala@domain.fi>
2025-04-28 10:16:32 +0300

    uusi fontti

--- src/lib/components/WeatherMain.svelte
+++ src/lib/components/WeatherMain.svelte
@@ -15,39 +15,27 @@
 	let { weather, returnedCity }: Props = $props();
 </script>
 
-<<<<<<< HEAD
-<h2>{returnedCity}</h2>
-{#if weather}
-	<p>Lämpötila {weather.current.temperature_2m} °C</p>
-	<p>Tuntuu kuin {weather.current.apparent_temperature} °C</p>
-	<p>Suhteellinen ilmankosteus {weather.current.relative_humidity_2m} %</p>
-	<p>Tuulennopeus {weather.current.wind_speed_10m} m/s</p>
-	<p>Tuulensuunta {weather.current.wind_direction_10m} °</p>
-	<p>Pilvisyys {weather.current.cloud_cover} %</p>
-	<p>{weather.current.is_day ? 'Päivä' : 'Yö'}</p>
-	<p>Sateen määrä {weather.current.rain} mm</p>
-{:else}
-	<p>Ei säätietoja</p>
-{/if}
-=======
 <div>
 	<h2>{returnedCity}</h2>
 	{#if weather}
-		<p>Temperature {weather.current.temperature_2m} °C</p>
-		<p>Apparent temperature {weather.current.apparent_temperature} °C</p>
-		<p>Relative humidity {weather.current.relative_humidity_2m} %</p>
-		<p>Wind speed {weather.current.wind_speed_10m} m/s</p>
-		<p>Wind Direction {weather.current.wind_direction_10m} °</p>
-		<p>Cloud cover {weather.current.cloud_cover} %</p>
-		<p>Now is {weather.current.is_day ? 'Day' : 'Night'}</p>
-		<p>Rain {weather.current.rain} mm</p>
+		<p>Lämpötila {weather.current.temperature_2m} °C</p>
+		<p>Tuntuu kuin {weather.current.apparent_temperature} °C</p>
+		<p>Suhteellinen kosteus {weather.current.relative_humidity_2m} %</p>
+		<p>Tuulen nopeus {weather.current.wind_speed_10m} m/s</p>
+		<p>Tuulen suunta {weather.current.wind_direction_10m} °</p>
+		<p>Pilvisyys {weather.current.cloud_cover} %</p>
+		<p>Nyt on {weather.current.is_day ? 'Päivä' : 'Yö'}</p>
+		<p>Sade {weather.current.rain} mm</p>
 	{:else}
 		<p>Ei säätietoja</p>
 	{/if}
 </div>
->>>>>>> f75faeccff4cf5ef738145a60dab3bc598691b98
 
 <style>
+	:global(:root) {
+		font-family: 'Inter', sans-serif;
+	}
+
 	div {
 		/*background-color: black;*/
 	}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,4 +26,8 @@
 		max-width: 240px;
 		margin: 6em auto 0 auto;
 	}
+	@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+	* {
+		font-family: 'Inter', sans-serif;
+	}
 </style>
