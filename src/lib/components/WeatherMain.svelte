f75faeccff4cf5ef738145a60dab3bc598691b98
Jarkko Remes <ag1534@student.jamk.fi>
2025-04-28 09:25:22 +0300

    Wrapped weathermain in a div

--- src/lib/components/WeatherMain.svelte
+++ src/lib/components/WeatherMain.svelte
@@ -15,21 +15,26 @@
 	let { weather, returnedCity }: Props = $props();
 </script>
 
-<h2>{returnedCity}</h2>
-{#if weather}
-	<p>Temperature {weather.current.temperature_2m} °C</p>
-	<p>Apparent temperature {weather.current.apparent_temperature} °C</p>
-	<p>Relative humidity {weather.current.relative_humidity_2m} %</p>
-	<p>Wind speed {weather.current.wind_speed_10m} m/s</p>
-	<p>Wind Direction {weather.current.wind_direction_10m} °</p>
-	<p>Cloud cover {weather.current.cloud_cover} %</p>
-	<p>Now is {weather.current.is_day ? 'Day' : 'Night'}</p>
-	<p>Rain {weather.current.rain} mm</p>
-{:else}
-	<p>Ei säätietoja</p>
-{/if}
+<div>
+	<h2>{returnedCity}</h2>
+	{#if weather}
+		<p>Temperature {weather.current.temperature_2m} °C</p>
+		<p>Apparent temperature {weather.current.apparent_temperature} °C</p>
+		<p>Relative humidity {weather.current.relative_humidity_2m} %</p>
+		<p>Wind speed {weather.current.wind_speed_10m} m/s</p>
+		<p>Wind Direction {weather.current.wind_direction_10m} °</p>
+		<p>Cloud cover {weather.current.cloud_cover} %</p>
+		<p>Now is {weather.current.is_day ? 'Day' : 'Night'}</p>
+		<p>Rain {weather.current.rain} mm</p>
+	{:else}
+		<p>Ei säätietoja</p>
+	{/if}
+</div>
 
 <style>
+	div {
+		/*background-color: black;*/
+	}
 	h2 {
 		color: var(--main-color);
 	}
