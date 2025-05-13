1cbaed31af00d323db44dd093a908da3f52cdbe6
Jarkko Remes <ag1534@student.jamk.fi>
2025-05-13 21:19:47 +0300

    Fixed issue, where hour list would be empty after 21

--- src/lib/weatherGlobal.svelte.ts
+++ src/lib/weatherGlobal.svelte.ts
@@ -164,7 +164,7 @@ class WeatherGlobal {
 			let loop = false;
 			const tietoTaulukko: Weather[] = [];
 			for (const tieto of this.saatietoTaulukko) {
-				if (new Date().getUTCHours() < 12 || new Date().getUTCHours() >= 21) {
+				if (new Date().getUTCHours() < 12 || new Date().getHours() >= 21) {
 					if (tieto.Date.getUTCHours() === 12) {
 						tietoTaulukko.push(tieto);
 					}
@@ -178,8 +178,8 @@ class WeatherGlobal {
 				}
 				loop = true;
 			}
-			// console.log('PäiväTaulukko:');
-			// console.log(tietoTaulukko);
+			console.log('PäiväTaulukko:');
+			console.log(tietoTaulukko);
 			return tietoTaulukko;
 		}
 		return [];
